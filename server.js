557177d47dcc394c0c835513f569635aaa32d84d
Shukti <nazrinshukti@gmail.com>
2020-04-17 00:45:52 +0600

    ADD project page

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port=process.env.PORT||3000;
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -14,7 +13,7 @@ app.use((req, res, next) => {
   var log = `${now}: ${req.method} ${req.url}`;
 
   console.log(log);
-  fs.appendFileSync('server.log', log + '\n');
+  fs.appendFile('server.log', log + '\n');
   next();
 });
 
@@ -42,6 +41,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
